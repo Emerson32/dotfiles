ef48449ed1b4654adec831cccd7e8e3d70c4ab46
Emerson <Houndlover32@gmail.com>
2019-06-09 18:38:35 -0400

    Added necessary global_conf.py

---
+++ .config/nvim/nvim_configs/global_extra_conf.py
@@ -0,0 +1,8 @@
+# Place this in the user's home directory
+
+def Settings( **kwargs ):
+  client_data = kwargs[ 'client_data' ]
+  return {
+    'interpreter_path': client_data[ 'g:ycm_python_interpreter_path' ],
+    'sys_path': client_data[ 'g:ycm_python_sys_path' ]
+  }
